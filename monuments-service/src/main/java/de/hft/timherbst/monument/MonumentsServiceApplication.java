eba7ee72fa12ed6b89138c398449596aa2ee1613
Tim Herbst <herbst.tim@icloud.com>
2022-12-26 17:15:56 +0100

    Add main class

---
+++ monuments-service/src/main/java/de/hft/timherbst/monument/MonumentsServiceApplication.java
@@ -0,0 +1,13 @@
+package de.hft.timherbst.monument;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonumentsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonumentsServiceApplication.class, args);
+    }
+
+}
